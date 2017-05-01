bcc3ec84e64602ab341586db339bafbe2eadf7c2
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2017-04-30 20:55:16 -0400

    Adding javascript for loading view dynamically

---
+++ scripts/load_content.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  var client = new XMLHttpRequest();
+  client.open('GET', 'views/html/home.html');
+  client.onreadystatechange = function() {
+    document.getElementById("main-content").innerHTML = client.responseText;
+  };
+  client.send();
+};
