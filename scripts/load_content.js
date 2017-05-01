1d189f6dfc08ad99ec5f8393704fe4049b57806a
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2017-05-01 14:01:36 -0400

    Implementing dynamic loading and rendering of pages

--- scripts/load_content.js
+++ scripts/load_content.js
@@ -1,8 +1,36 @@
 window.onload = function() {
+  var mainContentDiv = document.getElementById("main-content");
+
+  mainContentDiv.style.width = Math.max(Math.min(window.innerWidth, window.innerHeight), 0.6 * window.innerWidth) + "px";
+
+  var page = getURLParameter("page");
+  var blog = getURLParameter("entry");
+
+  if (!page)
+    page = "home.html";
+
+  var renderFile = "views/html/" + page;
+  if (page === "blog.html" && blog)
+    renderFile = "views/markdown/" + blog;
+
+  var md = window.markdownit();
+
   var client = new XMLHttpRequest();
-  client.open('GET', 'views/headless_html/home.html');
+  client.open("GET", renderFile);
   client.onreadystatechange = function() {
-    document.getElementById("main-content").innerHTML = client.responseText;
+    if (client.readyState === 4) {
+      if (client.status === 200) {
+        var renderText = renderFile.split(".").pop() === "md" ? md.render(client.responseText) : client.responseText;
+        mainContentDiv.innerHTML += renderText;
+      } else {
+        mainContentDiv.innerHTML += "<h1>Page not found.</h1>";
+      }
+    }
   };
   client.send();
 };
+
+/// Taken from http://stackoverflow.com/a/11582513
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
---
+++ views/markdown/hello-world.md
@@ -0,0 +1,14 @@
+# First entry!
+
+Hi! My name is Nick and this is the first entry into my personal website's blog.
+The significance of this event is not so much the birth of the blog itself, but
+the accomplishment of developing the blog system. I had the idea to make use of
+Github Pages to make a very simple blog system in which updates could be made by
+simply pushing new markdown files to the repository. I saw this as a nice little
+challenge considering Github Pages doesn't support hosting real web apps, but
+only static HTML pages. Thus, my hacking was very experimental and purely a
+learning experience. Nevertheless, from the experience also came a little place
+I could call my own on the internet -- a place into which I could post whatever
+I felt like. So here is to a future of untold adventures.
+
+Cheers!
