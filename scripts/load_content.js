1993717cc1ff630d2e16d4c77a9fc120c9e369da
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2017-04-30 21:17:31 -0400

    Adding document metadata

--- scripts/load_content.js
+++ scripts/load_content.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var client = new XMLHttpRequest();
-  client.open('GET', 'views/html/home.html');
+  client.open('GET', 'views/headless_html/home.html');
   client.onreadystatechange = function() {
     document.getElementById("main-content").innerHTML = client.responseText;
   };
