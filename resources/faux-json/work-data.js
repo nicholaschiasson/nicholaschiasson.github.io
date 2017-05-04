ae8be97bc46381e4194e5ed3a421a5fbfe2d20f5
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2017-05-04 02:02:32 -0400

    Rendering work page projects

--- resources/faux-json/work-data.js
+++ resources/faux-json/work-data.js
@@ -2,61 +2,139 @@ WorkData = {
   "educationalProjects": [
     {
       "title": "checkers",
-      "url": "https://nicholaschiasson.github.io/checkers"
+      "description": "COMP4501 Assignment 1 - A simple local multiplayer game of checkers made with Unity.",
+      "thumbnail_url": "",
+      "home_url": "https://nicholaschiasson.github.io/checkers",
+      "download_url": "",
+      "repo_url": "https://github.com/nicholaschiasson/checkers",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "d00m3d",
-      "url": "https://nicholaschiasson.github.io/d00m3d"
+      "description": "COMP3501 Final Project - A space shooter game.",
+      "thumbnail_url": "",
+      "home_url": "https://nicholaschiasson.github.io/d00m3d",
+      "download_url": "https://github.com/nicholaschiasson/d00m3d/releases/latest",
+      "repo_url": "https://github.com/nicholaschiasson/d00m3d",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "laser-pong",
-      "url": "https://nicholaschiasson.github.io/laser-pong"
+      "description": "COMP1405 Assignment 7 - An arcade survival style game of Pong!",
+      "thumbnail_url": "",
+      "home_url": "https://nicholaschiasson.github.io/laser-pong",
+      "download_url": "https://github.com/nicholaschiasson/laser-pong/releases/latest",
+      "repo_url": "https://github.com/nicholaschiasson/laser-pong",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "PlanetCiv: S.P.A.C.E",
-      "url": "https://nicholaschiasson.github.io/PlanetCiv"
+      "description": "COMP1501 Final Project - A planetary colonization game!",
+      "thumbnail_url": "",
+      "home_url": "https://nicholaschiasson.github.io/PlanetCiv",
+      "download_url": "https://github.com/nicholaschiasson/PlanetCiv/releases/latest",
+      "repo_url": "https://github.com/nicholaschiasson/PlanetCiv",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "Snatch",
-      "url": "https://nicholaschiasson.github.io/Snatch"
+      "description": "COMP2501 Final Project - A puzzle solving escape game!",
+      "thumbnail_url": "",
+      "home_url": "https://nicholaschiasson.github.io/Snatch",
+      "download_url": "https://github.com/nicholaschiasson/Snatch/releases/latest",
+      "repo_url": "https://github.com/nicholaschiasson/Snatch",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "snatch3d-vr",
-      "url": "https://nicholaschiasson.github.io/snatch3d-vr"
+      "description": "Computer Science Undergraduate Honours Project - A virtual reality puzzle platformer game!",
+      "thumbnail_url": "",
+      "home_url": "https://nicholaschiasson.github.io/snatch3d-vr",
+      "download_url": "",
+      "repo_url": "https://github.com/nicholaschiasson/snatch3d-vr",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     }
   ],
   "personalProjects": [
     {
       "title": "car",
-      "url": "https://github.com/nicholaschiasson/car#car"
+      "description": "Compile and run! A command-line utility for quickly executing files containing code in a compiled language such as C, C++, Java, etc. Seamlessly executes files as if they were scripts, leaving no trace of binaries behind.",
+      "thumbnail_url": "http://www.misskatecuttables.com/uploads/shopping_cart/7778/large_car.png",
+      "home_url": "",
+      "download_url": "https://github.com/nicholaschiasson/car/releases/latest",
+      "repo_url": "https://github.com/nicholaschiasson/car",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "love-maker",
-      "url": "https://nicholaschiasson.github.io/love-maker"
+      "description": "A simple distribution building script for your LÖVE games!",
+      "thumbnail_url": "https://avatars.githubusercontent.com/u/10260207?size=200",
+      "home_url": "https://nicholaschiasson.github.io/love-maker",
+      "download_url": "",
+      "repo_url": "https://github.com/nicholaschiasson/love-maker",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-GPL--3.0-blue.svg"
     },
     {
       "title": "W3DL",
-      "url": "https://nicholaschiasson.github.io/W3DL"
+      "description": "Web 3D Library. An experimental graphics and math library built on WebGL.",
+      "thumbnail_url": "",
+      "home_url": "",
+      "download_url": "",
+      "repo_url": "https://github.com/nicholaschiasson/W3DL",
+      "doc_url": "https://nicholaschiasson.github.io/W3DL",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     }
   ],
   "jams": [
     {
-      "title": "Ludum Dare 31",
-      "url": "https://nicholaschiasson.github.io/KillTheSnowman"
+      "title": "Kill The Snowman",
+      "description": "Ludum Dare 31 entry - The title is a lie! Snowman is the best theme!",
+      "thumbnail_url": "http://images.clipartpanda.com/holiday-snowman-clip-art-snowman.png",
+      "home_url": "https://nicholaschiasson.github.io/KillTheSnowman",
+      "download_url": "https://github.com/nicholaschiasson/KillTheSnowman/releases/latest",
+      "repo_url": "https://github.com/nicholaschiasson/KillTheSnowman",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     }
   ],
   "gists": [
     {
       "title": "cdmod",
-      "url": "https://gist.github.com/nicholaschiasson/18edfbbddc5ea8188a239dcb17159bde"
+      "description": "A small set of bash aliases to effectively add backward and forward change directory functionality to the terminal, similar to how internet browser backward and forward navigation buttons work.",
+      "thumbnail_url": "",
+      "home_url": "",
+      "download_url": "",
+      "repo_url": "https://gist.github.com/nicholaschiasson/18edfbbddc5ea8188a239dcb17159bde",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "ls_hidden",
-      "url": "https://gist.github.com/nicholaschiasson/e2712d020a6317779555274f58b70f2f"
+      "description": "Some bash functions and aliases to more easily list hidden dot files and directories.",
+      "thumbnail_url": "",
+      "home_url": "",
+      "download_url": "",
+      "repo_url": "https://gist.github.com/nicholaschiasson/e2712d020a6317779555274f58b70f2f",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     },
     {
       "title": "MemMon",
-      "url": "https://gist.github.com/nicholaschiasson/85cd296f34c5a2728c5a61fb426242f8"
+      "description": "Memory monitors for C++ aiming to prevent double frees.",
+      "thumbnail_url": "",
+      "home_url": "",
+      "download_url": "",
+      "repo_url": "https://gist.github.com/nicholaschiasson/85cd296f34c5a2728c5a61fb426242f8",
+      "doc_url": "",
+      "license_badge": "https://img.shields.io/badge/license-MIT-blue.svg"
     }
   ]
 }
--- scripts/work.js
+++ scripts/work.js
@@ -3,11 +3,120 @@
 function renderWorkData() {
   function createWorkDataListItem(data) {
     let listItem = document.createElement("li");
-    let link = document.createElement("a");
-    link.setAttribute("target", "_blank");
-    link.setAttribute("href", data.url);
-    link.innerHTML = data.title;
-    listItem.appendChild(link);
+    let topDiv = document.createElement("div");
+    topDiv.setAttribute("class", "top");
+    if (data.title) {
+      listItem.setAttribute("id", "work-" + data.title);
+      let heading = document.createElement("h5");
+      heading.setAttribute("class", "left");
+      heading.innerHTML = data.title;
+      topDiv.appendChild(heading);
+    }
+    let topRightDiv = document.createElement("div");
+    topRightDiv.setAttribute("class", "right");
+    if (data.home_url) {
+      let link = document.createElement("a");
+      link.setAttribute("class", "left");
+      link.setAttribute("target", "_blank");
+      link.setAttribute("data-toggle", "tooltip");
+      link.setAttribute("title", "project home page");
+      link.setAttribute("href", data.home_url);
+      let objImg = document.createElement("object");
+      objImg.setAttribute("class", "svg-img");
+      objImg.setAttribute("data", "images/svg/octicons/home.svg");
+      objImg.setAttribute("type", "image/svg+xml");
+      objImg.setAttribute("width", "16px");
+      objImg.setAttribute("height", "16px");
+      link.appendChild(objImg);
+      topRightDiv.appendChild(link);
+    }
+    if (data.download_url) {
+      let link = document.createElement("a");
+      link.setAttribute("class", "left");
+      link.setAttribute("target", "_blank");
+      link.setAttribute("data-toggle", "tooltip");
+      link.setAttribute("title", "project downloads");
+      link.setAttribute("href", data.download_url);
+      let objImg = document.createElement("object");
+      objImg.setAttribute("class", "svg-img");
+      objImg.setAttribute("data", "images/svg/octicons/cloud-download.svg");
+      objImg.setAttribute("type", "image/svg+xml");
+      objImg.setAttribute("width", "16px");
+      objImg.setAttribute("height", "16px");
+      link.appendChild(objImg);
+      topRightDiv.appendChild(link);
+    }
+    if (data.repo_url) {
+      let link = document.createElement("a");
+      link.setAttribute("class", "left");
+      link.setAttribute("target", "_blank");
+      link.setAttribute("data-toggle", "tooltip");
+      link.setAttribute("title", "project repository");
+      link.setAttribute("href", data.repo_url);
+      let objImg = document.createElement("object");
+      objImg.setAttribute("class", "svg-img");
+      objImg.setAttribute("data", "images/svg/octicons/mark-github.svg");
+      objImg.setAttribute("type", "image/svg+xml");
+      objImg.setAttribute("width", "16px");
+      objImg.setAttribute("height", "16px");
+      link.appendChild(objImg);
+      topRightDiv.appendChild(link);
+    }
+    if (data.doc_url) {
+      let link = document.createElement("a");
+      link.setAttribute("class", "left");
+      link.setAttribute("target", "_blank");
+      link.setAttribute("data-toggle", "tooltip");
+      link.setAttribute("title", "project documentation");
+      link.setAttribute("href", data.doc_url);
+      let objImg = document.createElement("object");
+      objImg.setAttribute("class", "svg-img");
+      objImg.setAttribute("data", "images/svg/octicons/book.svg");
+      objImg.setAttribute("type", "image/svg+xml");
+      objImg.setAttribute("width", "16px");
+      objImg.setAttribute("height", "16px");
+      link.appendChild(objImg);
+      topRightDiv.appendChild(link);
+    }
+    let permalink = document.createElement("a");
+    permalink.setAttribute("class", "left");
+    permalink.setAttribute("data-toggle", "tooltip");
+    permalink.setAttribute("title", "permanent link");
+    permalink.setAttribute("href", "#work-" + data.title);
+    let objImg = document.createElement("object");
+    objImg.setAttribute("class", "svg-img");
+    objImg.setAttribute("data", "images/svg/octicons/link.svg");
+    objImg.setAttribute("type", "image/svg+xml");
+    objImg.setAttribute("width", "16px");
+    objImg.setAttribute("height", "16px");
+    permalink.appendChild(objImg);
+    topRightDiv.appendChild(permalink);
+    topDiv.appendChild(topRightDiv);
+    listItem.appendChild(topDiv);
+    let middleDiv = document.createElement("div");
+    middleDiv.setAttribute("class", "middle");
+    if (data.description) {
+      let paragraph = document.createElement("p");
+      paragraph.setAttribute("class", "left");
+      paragraph.innerHTML = data.description;
+      middleDiv.appendChild(paragraph);
+    }
+    if (data.thumbnail_url) {
+      let thumbnail = document.createElement("img");
+      thumbnail.setAttribute("class", "right");
+      thumbnail.setAttribute("src", data.thumbnail_url);
+      middleDiv.appendChild(thumbnail);
+    }
+    listItem.appendChild(middleDiv);
+    let bottomDiv = document.createElement("div");
+    bottomDiv.setAttribute("class", "bottom");
+    if (data.license_badge) {
+      let licenseBadge = document.createElement("img");
+      licenseBadge.setAttribute("class", "left");
+      licenseBadge.setAttribute("src", data.license_badge);
+      bottomDiv.appendChild(licenseBadge);
+    }
+    listItem.appendChild(bottomDiv);
     return listItem;
   }
 
