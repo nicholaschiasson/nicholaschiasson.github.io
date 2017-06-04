aa92626925b22f42a162cd00ef0841a68e70fa3d
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2017-06-04 15:00:32 -0400

    Base layout for home page

---
+++ scripts/root/home.js
@@ -0,0 +1,11 @@
+var tagline;
+
+function homeInitialization() {
+  tagline = document.getElementById("tagline");
+}
+
+function animateTagline() {
+
+}
+
+window.addEventListener("initialized", homeInitialization, true);
