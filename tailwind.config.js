15ce2cf075608ab175f50f525c56c882eb20892a
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2023-08-09 12:36:26 -0400

    Use fontawesome font files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans, "Font Awesome"],
+        serif: [...defaultTheme.fontFamily.serif, "Font Awesome"],
+      },
+    },
   },
   plugins: [],
 };
