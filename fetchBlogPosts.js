cb1cf3dc41c31ceb5bdc5c25e6e20b41e8011715
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2023-08-19 12:50:55 -0400

    Trying to explicitly define this environment variable

--- fetchBlogPosts.js
+++ fetchBlogPosts.js
@@ -6,7 +6,7 @@ const REPOSITORY_NAME = "nicholaschiasson.github.io";
 const DISCUSSION_CATEGORY_SLUG = "blog-posts";
 
 async function query(query) {
-  const res = await (
+  return await (
     await fetch("https://api.github.com/graphql", {
       method: "POST",
       headers: {
@@ -17,8 +17,6 @@ async function query(query) {
       body: JSON.stringify({ query }),
     })
   ).json();
-  console.error(res);
-  return res;
 }
 
 async function queryList(buildQuery, accessData) {
