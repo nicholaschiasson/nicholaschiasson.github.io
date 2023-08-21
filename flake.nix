4be4e9cabef5420d9eba3a9e754da326e74e732f
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2023-08-21 17:16:43 -0400

    Oh my goodness I am tripping over myself left and right

--- flake.nix
+++ flake.nix
@@ -42,5 +42,14 @@
           pkgs.nodePackages.tailwindcss
         ];
       };
+
+      devShells.ciBlog = pkgs.mkShell {
+        buildInputs = [
+          pkgs.deno
+          pkgs.gomplate
+          pkgs.jaq
+          pkgs.nodePackages.tailwindcss
+        ];
+      };
     });
 }
